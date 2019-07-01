ac2fac662d5a75c2ae939124383cd290a49b9e64
a-c-sreedhar-reddy <acreddysreedharreddy@gmail.com>
2019-07-02 02:09:50 +0530

    added useAsyncValue hook

---
+++ useAsyncValue.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
+export default function useValue(key) {
+  const [loading, setLoading] = useState(true);
+  const [value, setValue] = useState(null);
+  function getAsyncStorageValue() {
+    AsyncStorage.getItem(key)
+      .then(value => {
+        setValue(value);
+      })
+      .catch(() => setValue(null))
+      .finally(() => setLoading(false));
+  }
+  const setAsyncStorageValue = value => {
+    AsyncStorage.setItem(key, value).finally(getItem);
+  };
+  useEffect(getAsyncStorageValue, [key]);
+  function removeAsyncStorageKey() {
+    AsyncStorage.removeItem(key).finally(getAsyncStorageValue);
+  }
+  return [loading, value, setAsyncStorageValue, removeAsyncStorageKey];
+}
